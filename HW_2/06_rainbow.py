1888908d78b3b85d941f6b3687070675a0dbb09a
Ruslan Gubaydullin <Gubaidullin.r@bk.ru>
2020-05-13 13:48:03 +0400

    completed

--- HW_2/06_rainbow.py
+++ HW_2/06_rainbow.py
@@ -8,11 +8,14 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+point = sd.get_point(800,-50)
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-# TODO здесь ваш код
+radius = 450
+for i in rainbow_colors:
+    sd.circle(point,radius,i,25)
+    radius+=25
 
 sd.pause()
