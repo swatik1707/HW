9e2ec5ce45c14261d337b34aa4743d678d238f39
Ruslan Gubaydullin <Gubaidullin.r@bk.ru>
2020-05-14 18:26:25 +0400

    complete

--- HW_3/01_shapes.py
+++ HW_3/01_shapes.py
@@ -27,7 +27,87 @@ import simple_draw as sd
 # sd.line()
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
-# TODO здесь ваш код
+point_3angle = sd.get_point(100, 50)
+point_4angle = sd.get_point(100,300)
+point_6angle = sd.get_point(400, 50)
+point_5angle = sd.get_point(400, 300)
+
+def optiangle(angle_count, point, angle, length, step):
+    if angle_count < 1:
+        return
+    v1 = sd.get_vector(start_point=point, angle=angle, length = length, width=3)
+    v1.draw()
+
+    optiangle(angle_count = angle_count-1, point=v1.end_point, angle=angle+step, length = length, step=step)
+
+optiangle(3, point_3angle, 30, 75, 120)
+optiangle(4, point_4angle, 30, 75, 90)
+optiangle(5, point_5angle, 30, 75, 72)
+optiangle(6, point_6angle, 30, 75, 60)
+
+
+# def triangle(point, angle, length):
+#     v1 = sd.get_vector(start_point=point, angle=angle, length = length, width=3)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length = length, width=3)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length = length, width=3)
+#     v3.draw()
+# triangle(point_3angle,30,50)
+
+# def rectangle(point, angle, length):
+#     v1 = sd.get_vector(start_point=point, angle=angle, length = length, width=3)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length = length, width=3)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length = length, width=3)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 270, length = length, width=3)
+#     v4.draw()
+# rectangle(point_4angle,30,50)
+#
+# def fiveangle(point, angle, length):
+#     v1 = sd.get_vector(start_point=point, angle=angle, length = length, width=3)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length = length, width=3)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 144, length = length, width=3)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 216, length = length, width=3)
+#     v4.draw()
+#
+#     v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 288, length = length, width=3)
+#     v5.draw()
+# fiveangle(point_5angle,30,50)
+#
+# def sixangle(point, angle,length):
+#     v1 = sd.get_vector(start_point=point, angle=angle, length = length, width=3)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length = length, width=3)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length = length, width=3)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length = length, width=3)
+#     v4.draw()
+#
+#     v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length = length, width=3)
+#     v5.draw()
+#
+#     v6 = sd.get_vector(start_point=v5.end_point, angle=angle + 300, length = length, width=3)
+#     v6.draw()
+# sixangle(point_6angle,30,50)
+
 
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
