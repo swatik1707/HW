2278862207054ddcb3d196c09eada85b7328f720
swatik <swatik777@yandex.ru>
2020-05-21 18:51:01 +0400

    init

---
+++ temor/prime_numbers_usual.py
@@ -0,0 +1,12 @@
+max_num = 2**14
+prime_list = []
+for i in range(2,max_num):
+    k = 0
+    for t in range(2,i):
+        if i%t == 0:
+            k+=1
+        else:
+            continue
+    if k==0:
+        prime_list.append(i)
+print('Gotovo!')
