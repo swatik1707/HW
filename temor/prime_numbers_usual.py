883be09a4291b036152d1e5c64bf3f89e7987528
swatik <swatik777@yandex.ru>
2020-06-01 07:07:38 +0400

    edited

--- temor/prime_numbers_usual.py
+++ temor/prime_numbers_usual.py
@@ -1,6 +1,6 @@
-max_num = 2**13
+max_num = 2**14
 prime_list = []
-for i in range(2,max_num):
+for i in range(2,max_num+1):
     k = 0
     for t in range(2,i):
         if i%t == 0:
@@ -10,3 +10,4 @@ for i in range(2,max_num):
     if k==0:
         prime_list.append(i)
 print('Gotovo!')
+print(prime_list)
